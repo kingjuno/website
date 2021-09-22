5379ab2e46b1e4ca7141a7b945d8f0195eec52f2
kingjuno <geojollyc@gmail.com>
2021-09-22 12:01:51 +0530

    Add responsive navbar

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -18,3 +18,26 @@ hand.classList.add('wave');
 hand.addEventListener('mouseout', function() {
 hand.classList.remove('wave');
 });
+
+$('.toggled-nav-wrapper').on('click', function(e){
+    e.stopPropagation();
+  });
+
+$('.nav-toggle-open-wrapper, .nav-toggle-close-wrapper, header .nav-toggle-body-overlay').on('click', function(){
+  $('html').toggleClass('nav-open');
+});
+//Smooth Scroll
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
