0eb8e4a4efc8528e25d0ecce19f55ffb6d022d29
kingjuno <geojollyc@gmail.com>
2021-09-25 23:28:41 +0530

    Change navbar to sticky

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -41,3 +41,14 @@ $(function() {
     }
   });
 });
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+}
